c2a92bffb96912b20b6cd1628b7a4c9819b46b79
luke.maarman <luke.maarman@Xiamo.Traderoot.local>
2022-06-17 13:30:03 +0200

    commit chromedriver

--- Automation_Testing/src/test/java/PGWAutomationFramework/PostmanTest.java
+++ Automation_Testing/src/test/java/PGWAutomationFramework/PostmanTest.java
@@ -5,6 +5,7 @@ import java.awt.AWTException;
 public class PostmanTest {
 
 	public static void main(String[] args) throws AWTException, InterruptedException {
+		 
 		mCommerceTest.mCommerceSOVPayment();
 
 	}
