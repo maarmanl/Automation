3c3305f83a2c7c3a7b5b68b777f25b10efef021c
luke.maarman <luke.maarman@Xiamo.Traderoot.local>
2022-08-03 07:28:31 +0200

    page objects update

--- Automation_Testing/src/test/java/PGWAutomationFramework/PostmanTest.java
+++ Automation_Testing/src/test/java/PGWAutomationFramework/PostmanTest.java
@@ -8,15 +8,15 @@ public class PostmanTest {
 
 //		for (int i = 0; i < 5; i++) {
 
-//			eCommerceTest.ImmediatePayment();
+			eCommerceTest.ImmediatePayment();
 //			eCommerceTest.InitiateImmediateSplitPaymentAddCard();
 //			eCommerceTest.InitiateImmediateSplitPayment();
 //			eCommerceTest.ImmediateDelayedPayment();
 //			eCommerceTest.ImmediateDelayedSplitPayment(); 
 //			eCommerceTest.InitiateRecurringPayment(); 
-			mCommerceTest.listTokens();
-			mCommerceTest.mCommerceSOVPayment(); 
-			mCommerceTest.mCommerceCardPayment();
+//			mCommerceTest.listTokens();
+//			mCommerceTest.mCommerceSOVPayment(); 
+//			mCommerceTest.mCommerceCardPayment();
 //		}
 
 
